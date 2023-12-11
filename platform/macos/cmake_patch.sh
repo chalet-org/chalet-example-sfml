b6f92d1a088ea0d936deeec3b3a928bc23911c85
Andrew King <pepsiotaku@gmail.com>
2023-12-10 22:07:47 -0500

    macos: patch out DEBUG_POSTFIX so xcode project can use the same dylib filename between configurations

--- platform/macos/cmake_patch.sh
+++ platform/macos/cmake_patch.sh
@@ -2,4 +2,7 @@
 
 SFML_PATH="$1"
 
+perl -i -pe 's/.*/\t\t\tset_target_properties(${target} PROPERTIES DEBUG_POSTFIX "")/ if $.==98' "$SFML_PATH/cmake/Macros.cmake"
 perl -i -pe 's/.*/\tif(NOT SFML_OS_ANDROID AND NOT SFML_OS_MACOSX)/ if $.==115' "$SFML_PATH/cmake/Macros.cmake"
+
+
