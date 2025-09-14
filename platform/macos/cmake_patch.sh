6198be1f1cb2fb5db8fa93cfc8fec57cdc978734
Andrew King <pepsiotaku@gmail.com>
2025-09-14 19:20:05 -0400

    remove SFML2 Cmake patch, mac config

--- platform/macos/cmake_patch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SFML_PATH="$1"
-
-perl -i -pe 's/.*/\t\t\tset_target_properties(${target} PROPERTIES DEBUG_POSTFIX "")/ if $.==98' "$SFML_PATH/cmake/Macros.cmake"
-perl -i -pe 's/.*/\tif(NOT SFML_OS_ANDROID AND NOT SFML_OS_MACOSX)/ if $.==115' "$SFML_PATH/cmake/Macros.cmake"
-
-
